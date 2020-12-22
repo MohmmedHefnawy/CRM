107a422727aea7e7669c7b61e9c574babccb686a
3omrallam <76155662+3omrallam@users.noreply.github.com>
2020-12-22 15:31:47 +0200

    CRM 0.1

---
+++ src/app/dash.ts
@@ -0,0 +1,65 @@
+<!-- tasks-Card-progress -->
+<div class="container-fluid sM0-smoke-bg">
+  <div class="row ">
+    <div class="col">
+      <div class="container my-5">
+        <div class="row">
+          <div class="col-4 sM0-supp-0-bg text-center my-3 sM1-f-radius pb-3 px-0"> 
+            <!-- image -->
+            <div class="">
+              <img class="OM-Img-Task">
+            </div>
+            <!-- Title -->
+            <div class="sM0-supp-0-cr mt-2">
+              <p class="m-0 h5">Samana greens Avenue, Apartment</p>
+              <p class="m-0 h6">United Arab Emirats, Dubai</p>
+            </div>
+            <!-- Progress Employee -->
+            <div class="container">
+              <div class="row">
+                <div class="col sM0-supp-0-cr mb-2">
+                  <div class="col sM0-smoke-bg OM-Holder-Progress sM1-f-radius">
+                    <p class=" my-1 mt-4 d-flex justify-content-start">Graphic Designer</p>
+                    <div class="progress OM-Border-Reduis">
+                    <div class="progress-bar" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="col sM0-smoke-bg OM-Holder-Progress sM1-f-radius">
+                    <p class="my-1 mt-4 d-flex justify-content-start">Photographer</p>
+                    <div class="progress OM-Border-Reduis">
+                    <div class="progress-bar" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                  <div class="col sM0-smoke-bg OM-Holder-Progress sM1-f-radius">
+                    <p class="my-1 mt-4 d-flex justify-content-start">Project Manger</p>
+                    <div class="progress OM-Border-Reduis">
+                    <div class="progress-bar" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+/* TASK === START */
+
+.OM-Img-Task {
+  background-image: url("/assets/pexels-klas-tauberman-128362.jpg");
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 20px 20px 0px 0px;
+  height: 150px;
+  width: 100%;
+}
+.OM-Holder-Progress {
+  min-height: 60px;
+  max-height: 60px;
+}
+
+/* TASK === END */
