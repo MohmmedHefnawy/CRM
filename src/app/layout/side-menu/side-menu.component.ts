f825cac5d5c86e4d2d42bedf570a6d392a742cd0
Mohamed Elbehary <melbehary95@gmail.com>
2020-12-22 15:38:14 +0200

    CRM. 0.1
    Side Menu

--- src/app/layout/side-menu/side-menu.component.ts
+++ src/app/layout/side-menu/side-menu.component.ts
@@ -10,6 +10,22 @@ export class SideMenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // [?] resize the content container holder to fit the window height size
+    function resizeContentHolder(holder) {
+      var innerWidth = window.innerWidth;
+      var innerHeightWithoutNav = window.innerHeight;
+      if (innerWidth < 992) {
+        $(`#${holder}`).css("height", innerHeightWithoutNav);
+      } else {
+        if (innerHeightWithoutNav > 700) {
+          $(`#${holder}`).css("height", innerHeightWithoutNav);
+        } else {
+          $(`#${holder}`).css("height", 700);
+        }
+      }
+    }
+    // [?] on load at ther realtime
+    resizeContentHolder('sideMenuHolder');
   }
 
 }
