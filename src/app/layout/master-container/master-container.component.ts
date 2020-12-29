2ba94e77a99366aa33ef0bc1ce89eedaeb547ccf
Mohamed Elbehary <melbehary95@gmail.com>
2020-12-29 15:29:45 +0200

    CRM 0.1
    Mater Container

--- src/app/layout/master-container/master-container.component.ts
+++ src/app/layout/master-container/master-container.component.ts
@@ -10,6 +10,28 @@ export class MasterContainerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let holderWindowSize = (holder) => {// [?] holder is the selected holder <div> fo the component
+      // [?] window reisze event | to resize holder every Browser winodw resize
+      window.addEventListener("resize", function () {
+        resizeContentHolder(holder);
+      });
+      // [?] resize the content container holder to fit the window height size
+      function resizeContentHolder(elem) {
+        var innerWidth = window.innerWidth;
+        var innerHeightWithoutNav = window.innerHeight;
+        if (innerWidth < 992) {
+          $(elem).css("height", innerHeightWithoutNav);
+        } else {
+          if (innerHeightWithoutNav > 700) {
+            $(elem).css("height", innerHeightWithoutNav);
+          } else {
+            $(elem).css("height", 700);
+          }
+        }
+      }
+      // [?] on load at ther realtime
+      resizeContentHolder(holder);
+    }
+    holderWindowSize('#masterContainer')
   }
-
 }
