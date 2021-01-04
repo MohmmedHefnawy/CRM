0b13d9d5c8715bbe5fdc6207c046a43082afbb2b
3omrallam <76155662+3omrallam@users.noreply.github.com>
2021-01-04 20:50:59 +0200

    CRM 0.1

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -4,14 +4,27 @@ import { ProfileComponent } from './profile/profile.component';
 import { TeamsComponent }from './teams/teams.component'
 import { UserComponent } from './user.component'
 import { NotificationsComponent } from './notifications/notifications.component'
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PersonalInfoComponent } from './settings/personal-info/personal-info.component';
+import { ChangePasswordComponent } from './settings/change-password/change-password.component';
+import { ContactComponent } from './settings/contact/contact.component';
 const routes: Routes = [
   { path: 'user', redirectTo: 'user/profile', pathMatch: 'full' }, // redirect to `profile`
+  { path: 'user/setting', redirectTo: 'user/setting/personal-info', pathMatch: 'full' }, // redirect to `profile`
   {
     path: 'user', component: UserComponent,
     children: [
       { path: 'teams', component: TeamsComponent },
       { path: 'profile', component: ProfileComponent },
-      {path : 'notifi', component : NotificationsComponent}
+      {path : 'notifi', component : NotificationsComponent},
+      { path : 'create-account', component : CreateAccountComponent},
+      { path : 'setting' , component : SettingsComponent,
+       children: [
+        { path : 'personal-info' , component : PersonalInfoComponent},
+        { path : 'change-password' , component : ChangePasswordComponent},
+        { path : 'contact' , component : ContactComponent}
+      ]},
     ]
   },
 ];
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -6,10 +6,15 @@ import { TeamsComponent } from './teams/teams.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserComponent } from './user.component';
 import { NotificationsComponent } from './notifications/notifications.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { SettingsComponent } from './settings/settings.component';
+import { PersonalInfoComponent } from './settings/personal-info/personal-info.component';
+import { ChangePasswordComponent } from './settings/change-password/change-password.component';
+import { ContactComponent } from './settings/contact/contact.component';
 
 
 @NgModule({
-  declarations: [TeamsComponent, ProfileComponent, UserComponent, NotificationsComponent],
+  declarations: [TeamsComponent, ProfileComponent, UserComponent, NotificationsComponent, CreateAccountComponent, SettingsComponent, PersonalInfoComponent, ChangePasswordComponent, ContactComponent],
   exports: [UserRoutingModule],
   imports: [
     CommonModule,
