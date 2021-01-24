221f7c8efee055bc196cb7e66b52248783a1e844
Mohamed Elbehary <melbehary95@gmail.com>
2021-01-24 13:35:38 +0200

    CRM. 0.1
    ProgressBar

--- src/app/task/task-details/dash-board/dash-board.component.ts
+++ src/app/task/task-details/dash-board/dash-board.component.ts
@@ -10,6 +10,33 @@ export class DashBoardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // progress bar
+    $(function () {
+      $(".progress").each(function () {
+
+        const value = $(this).attr('data-value');
+        const left = $(this).find('.progress-left .progress-bar');
+        const right = $(this).find('.progress-right .progress-bar');
+
+        if (value > 0) {
+          if (value <= 50) {
+            right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
+          } else {
+            right.css('transform', 'rotate(180deg)')
+            left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
+          }
+        }
+
+      })
+
+      function percentageToDegrees(percentage) {
+
+        return percentage / 100 * 360
+
+      }
+
+    });
+
   }
 
 }
--- src/app/task/task-details/task-details.component.ts
+++ src/app/task/task-details/task-details.component.ts
@@ -11,31 +11,5 @@ export class TaskDetailsComponent implements OnInit {
   constructor() { }
   // tslint:disable-next-line: typedef
   ngOnInit() {
-    $(function () {
-      $(".progress").each(function () {
-
-        const value = $(this).attr('data-value');
-        const left = $(this).find('.progress-left .progress-bar');
-        const right = $(this).find('.progress-right .progress-bar');
-
-        if (value > 0) {
-          if (value <= 50) {
-            right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-          } else {
-            right.css('transform', 'rotate(180deg)')
-            left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
-          }
-        }
-
-      })
-
-      function percentageToDegrees(percentage) {
-
-        return percentage / 100 * 360
-
-      }
-
-    });
-
   }
 }
