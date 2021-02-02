f7a5c6b0ee0cb695aaccf699e7d330ef4566bcc1
3omrallam <76155662+3omrallam@users.noreply.github.com>
2021-02-02 17:15:04 +0200

    CRM 0.1

--- src/app/user/components/profile/profile.component.ts
+++ src/app/user/components/profile/profile.component.ts
@@ -1,3 +1,4 @@
+import { ProfileServiceService } from './../../services/profile-service.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/services/auth.service';
@@ -24,26 +25,50 @@ export class ProfileComponent implements OnInit {
     public navService: NavigatorServicesService,
     public authService: AuthService,
     public teamService: TeamsService,
+    public profileService: ProfileServiceService,
     private router: Router
   ) {
     console.log(this.router.url);
 
   }
+  fileToUpload: File = null;
 
   ngOnInit(): void {
     this.navService.navigators = this.navigator
+    this.uploadFileToActivity()
   }
-  onFileSelect(input) {
-    console.log(input.files);
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = (e: any) => {
-        console.log('Got here: ', e.target.result);
-        this.obj.photoUrl = e.target.result;
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+    console.log(this.fileToUpload);
 
   }
+
+  uploadFileToActivity() {
+    this.profileService.postPersonalImage(this.fileToUpload).subscribe(res => {
+      console.log(res);
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // onFileSelect(input) {
+
+  //   console.log(input.files);
+  //   if (input.files && input.files[0]) {
+  //     var reader = new FileReader();
+  //     reader.onload = (e: any) => {
+  //       console.log('Got here: ', e.target.result);
+  //     }
+  //     reader.readAsDataURL(input.files[0]);
+  //     this.profileService.postPersonalImage(input.files[0]).subscribe(res => {
+  //       console.log(res);
+  //     })
+  //     console.log('done');
+
+  //   }
+  // }
 
 }
+
+
