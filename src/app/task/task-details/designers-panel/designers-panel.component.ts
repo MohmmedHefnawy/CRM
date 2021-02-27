33cdbfb5aab0b2753b83746c1153f259d3658097
3omrallam <76155662+3omrallam@users.noreply.github.com>
2021-02-27 20:14:54 +0200

    CRM 0.1

--- src/app/task/task-details/designers-panel/designers-panel.component.ts
+++ src/app/task/task-details/designers-panel/designers-panel.component.ts
@@ -20,28 +20,67 @@ export class DesignersPanelComponent implements OnInit {
     this.socketON('Uploaded')
     this.socketON('Error_Uploaded')
   }
+
+  // ! Controller
+
   // Change input Value
-  changeValue(val) {
-    console.log(val.Package3DPhoto);
-    this.inpUploadDesigner = val.Package3DPhoto
-  }
-  // Handing Upload File
-  handleFileInput(files: FileList) {
-    let fileToUpload = files.item(0);
-    if (fileToUpload) {
-      this.uploadFileToActivity(fileToUpload)
+  boxModel(packageID) {
+    let data
+    return data = {
+      title: '',
+      image_link: '',
+      id: packageID
     }
   }
-  // Post Upload File To Server
-  uploadFileToActivity(uploadedFIle) {
-    //   this.taskDetailsService.postFileUpload(uploadedFIle).subscribe(res => {
-    //     console.log(res);
-    //   });
+  // ! API POST And PATCH
+  postPhotographerPackage() {
+    let data: any = {
+      PostID: this.taskDetailsService.propID
+    };
+    this.taskDetailsService.postPhotographerBoxRow(data).subscribe(res => {
+      console.log(res);
+      this.taskDetailsService.tour3DPackage.push(this.boxModel(res.id))
+      console.log(this.taskDetailsService?.tour3DPackage);
+    });
   }
-  // Listner To Data From Server
+  deletePhotographerPackage(boxRowID, index) {
+    this.taskDetailsService.deletePhotographerPackage(boxRowID).subscribe(res => {
+      console.log(res);
+    }, err => {
+      console.log(err);
+    }, () => {
+      this.taskDetailsService.tour3DPackage.splice(index, 1)
+
+    })
+  }
+  updatePhotographerPackage(boxRowID, title: any = false, file: any = false, currentStatus: any = false) {
+    let data: any = {};
+    // console.log(currentStatus);
+
+    if (file) {
+      let filePackage = file.item(0);
+      const formData: FormData = new FormData();
+      formData.append('file', filePackage);
+      console.log(formData)
+      // formData.append('unzip', 'false');
+      data = formData
+    } else if (currentStatus == 0 || currentStatus == 1) {
+      currentStatus == 1 ? data.current_status = '0' : data.current_status = '1'
+    } else if (title || title != '') {
+      data.title = title
+    }
+    this.taskDetailsService.updatePhotographerPackage(boxRowID, data).subscribe((res: any) => {
+      let updatedPackage = res.data
+      for (let package3D of this.taskDetailsService.tour3DPackage) {
+        package3D.id == updatedPackage.id ? package3D.current_status = updatedPackage.current_status : false
+      }
+      console.log(updatedPackage);
+
+    })
+  }
+  // ! Socket Handler
   socketON(listner) {
     this.socketService.socketON(listner).subscribe(res => {
-      this.changeValue(res)
       console.log(`Receiver From Designer Component : ...... ${res}`);
     }, err => {
     }, () => {
